34abd66bbb430ab17ecb3ff84bacb5f010efd594
Emmanuel Aguayo <eaguayo@elsol.local>
2023-10-26 09:27:46 -0300

    Vista con APIView para cada acción (crear, listar, consultar, modificar y eliminar)

--- aplicaciones/cuentas/views.py
+++ aplicaciones/cuentas/views.py
@@ -1,3 +1,24 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from aplicaciones.cuentas.serializers import *
+from aplicaciones.cuentas.models import *
 
 # Create your views here.
+"""
+    MOVIMIENTO
+        Create, Read, Update, Destroy
+"""
+class MovimientoCreateView(CreateAPIView):
+    serializer_class = MovimientoSerializers
+
+class MovimientoListView(ListAPIView):
+    queryset = Movimiento.objects.all()
+    serializer_class = MovimientoSerializers
+
+class MovimientoRetrieveView(RetrieveUpdateDestroyAPIView):
+    queryset = Movimiento.objects.all()
+    serializer_class = MovimientoSerializers
