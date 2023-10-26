d2464bb20c8aebb6e0b99274c527630c59d5e54a
Emmanuel Aguayo <eaguayo@elsol.local>
2023-10-26 09:58:28 -0300

    Endpoints crear-cliente, listar-cliente, editar-cliente, crear-moneda, listar-moneda, editar-moneda, listar-movimiento, bloaquear-cuenta

--- aplicaciones/cuentas/urls.py
+++ aplicaciones/cuentas/urls.py
@@ -1,5 +1,22 @@
 from django.urls import path, include
-from .views import *
+from . import views
 
 urlpatterns = [
+
+    # CLIENTE
+    path('crear-cliente/', views.ClienteCreateView.as_view()),
+    path('listar-cliente/', views.ClienteListView.as_view()),
+    path('editar-cliente/<int:pk>', views.ClienteRetrieveView.as_view()),
+
+    # MONEDA
+    path('crear-moneda/', views.MonedaCreateView.as_view()),
+    path('listar-moneda/', views.MonedaListView.as_view()),
+    path('editar-moneda/<int:pk>/', views.MonedaRetrieveView.as_view()),
+
+    # MOVIMIENTO
+    path('listar-cuenta/', views.CuentaListView.as_view()),
+
+    # CUENTA
+    path('bloquear-cuenta/<int:pk>', views.CuentaBloqueoView.as_view()),
+
 ]
