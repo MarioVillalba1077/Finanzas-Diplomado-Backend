5c8cf11fb2cac4939e68243fa26f9885e9401169
Mario Rubén Villalba <147746973+MarioVillalba1077@users.noreply.github.com>
2023-10-26 12:24:13 -0400

    En cuentas\views.py se agrega clase TransferenciaView

--- aplicaciones/cuentas/views.py
+++ aplicaciones/cuentas/views.py
@@ -1,3 +1,129 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+
+class TransferenciaView(APIView):
+
+    def post(self, request):
+
+        nro_cuenta_origen = request.data.get('nro_cuenta_origen')
+        nro_cuenta_destino = request.data.get('nro_cuenta_destino')
+        monto = request.data.get('monto')
+        canal_movimiento = request.data.get('canal')
+
+        # Validar datos necesarios
+
+        if not all([nro_cuenta_origen, nro_cuenta_destino, monto]):
+            return Response({"ok": False,
+                             "message": "La solicitud no contiene los datos necesarios"},
+                            status=status.HTTP_400_BAD_REQUEST)
+        # Validar Monto
+
+        try:
+            monto = float(monto)
+        except Exception:
+            return Response({"ok": False,
+                             "message": "Dato no válido para Monto"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Si se envió Canal validar el dato
+
+        if canal_movimiento is not None:
+            if canal_movimiento != "A" and canal_movimiento != "C" and canal_movimiento != "W":
+                return Response({"ok": False,
+                                 "message": "Dato no válido para Canal"},
+                                status=status.HTTP_400_BAD_REQUEST)
+        else:
+            canal_movimiento = "W"
+
+        # Recuperamos Cuenta de Origen
+
+        try:
+            cuenta_origen = Cuenta.objects.get(numero_cuenta=nro_cuenta_origen)
+        except Cuenta.DoesNotExist:
+            return Response({"ok": False,
+                             "message": "Cuenta de Origen no encontrada"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Recuperamos Cuenta de Destino
+
+        try:
+            cuenta_destino = Cuenta.objects.get(numero_cuenta=nro_cuenta_destino)
+        except Cuenta.DoesNotExist:
+            return Response({"ok": False,
+                             "message": "Cuenta de Destino no encontrada"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Validamos que la Cuenta de Origen esté activa
+
+        if cuenta_origen.estado != "A":
+            return Response({"ok": False,
+                             "message": "La Cuenta de Origen no está Activa"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Validamos que la Cuenta de Destino esté activa
+
+        if cuenta_destino.estado != "A":
+            return Response({"ok": False,
+                             "message": "La Cuenta de Destino no está Activa"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Validamos que las Cuentas tengan la misma Moneda
+
+        if cuenta_origen.moneda != cuenta_destino.moneda:
+            return Response({"ok": False,
+                             "message": "Las Cuentas no manejan la misma Moneda"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Validamos el Saldo de la Cuenta Origen
+
+        if cuenta_origen.saldo < monto:
+            return Response({"ok": False,
+                             "message": "El saldo de la cuenta es inferior al monto a Transferir"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Calcular saldos
+        saldo_actual_origen = cuenta_origen.saldo - monto
+        saldo_actual_destino = cuenta_destino.saldo + monto
+
+        # Registrar movimientos
+        Movimiento.objects.create(cuenta=cuenta_origen,
+                                  tipo_movimiento='DEB',
+                                  saldo_anterior=cuenta_origen.saldo,
+                                  saldo_actual=saldo_actual_origen,
+                                  monto_movimiento=monto,
+                                  numero_cuenta_origen=nro_cuenta_origen,
+                                  numero_cuenta_destino=nro_cuenta_destino,
+                                  canal=canal_movimiento)
+
+        Movimiento.objects.create(cuenta=cuenta_destino,
+                                  tipo_movimiento='CRE',
+                                  saldo_anterior=cuenta_destino.saldo,
+                                  saldo_actual=saldo_actual_destino,
+                                  monto_movimiento=monto,
+                                  numero_cuenta_origen=nro_cuenta_origen,
+                                  numero_cuenta_destino=nro_cuenta_destino,
+                                  canal=canal_movimiento)
+
+        # Actualizamos el Saldo de las Cuentas
+
+        cuenta_origen.saldo -= monto
+        cuenta_destino.saldo += monto
+        cuenta_origen.save()
+        cuenta_destino.save()
+
+        return Response({"ok": True,
+                         "message": "Transferencia Realizada con Éxito"},
+                        status=status.HTTP_200_OK)
+
+
+
+
+
