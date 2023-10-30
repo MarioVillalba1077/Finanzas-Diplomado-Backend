7fe19ea502a8198bb2bed23bf6b1e9edd1ef5385
Emmanuel Aguayo <eaguayo@elsol.local>
2023-10-30 09:48:53 -0300

    Se agrega la función get_queryset(self) para buscar clientes. Como los datos nombre y apellido se encuentran en la tabla persona se hace referencia primero a la llave foránea y luego al campo que se encuentra en la tabla principal en este caso => persona__nombre__icontains & persona__apellido_i_contains

--- aplicaciones/cuentas/views.py
+++ aplicaciones/cuentas/views.py
@@ -21,6 +21,18 @@ class ClienteListView(ListAPIView):
         queryset = Cliente.objects.all()
         serializer_class = ClienteSerializers
 
+class ClienteSearchView(ListAPIView):
+    serializer_class = ClienteSerializers
+
+    def get_queryset(self):
+        # Obtener el valor ingresado desde el request
+        kword = self.kwargs['kword']
+
+        # Filtrar clientes que coincidan con un nombre o apellido de la tabla persona
+        return Cliente.objects.filter(
+            Q(persona__nombre__icontains=kword) | Q(persona__apellido__icontains=kword)
+        )
+
 class ClienteRetrieveView(RetrieveUpdateDestroyAPIView):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializers
