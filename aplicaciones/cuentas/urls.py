e7d1f2b251bf17f4ffc41a218fcfd8d1184a5fce
Mario Rubén Villalba <147746973+MarioVillalba1077@users.noreply.github.com>
2023-11-06 12:13:44 -0400

    Actualizacion de archivo urls.py

--- aplicaciones/cuentas/urls.py
+++ aplicaciones/cuentas/urls.py
@@ -5,4 +5,12 @@ urlpatterns = [
     path('v1/transferencia', TransferenciaView.as_view()),
     path('v1/retiro', RetiroView.as_view()),
     path('v1/deposito', DepositoView.as_view()),
+
+    path('v1/listar-cuentas', CuentaListView.as_view()),
+    path('v1/buscar-cuentas-numero/<kword>', CuentaSearchForNumber.as_view()),
+    path('v1/buscar-cuentas-cliente/<kword>', CuentaSearchForCustomer.as_view()),
+    path('v1/crear-cuenta', CuentaCreateView.as_view()),
+    path('v1/modificar-eliminar-cuenta/<pk>', CuentaRetrieveView.as_view()),
+
+    path('v1/bloquear-cuenta/', CuentaBloqueoView.as_view()),
 ]
